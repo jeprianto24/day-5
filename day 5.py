2e6b93cc5a40ae05d473b71347fb58742d0c7fe2
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2022-10-23 17:54:02 +0800

    Add files via upload

---
+++ day 5.py
@@ -0,0 +1,9 @@
+print ("======================================")
+print ("   Biodata Sederhana Dengan Python   ")
+print ("======================================")
+ 
+nama = input("Masukkan nama: ")
+hobi = input("Masukkan hobi: ")
+umur = input("Inputkan umur: ")
+ 
+print ("Halo, Saya %s, hobi saya %s dan umur saya %s tahun" % (nama, hobi, umur))
